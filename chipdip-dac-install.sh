0f4aae6df2a4302b0ea2cd50162135cfd79ad38b
chipdip.lab <43340836+chipdipru@users.noreply.github.com>
2021-07-16 15:57:39 +0300

    Update chipdip-dac-install.sh

--- chipdip-dac-install.sh
+++ chipdip-dac-install.sh
@@ -4,7 +4,7 @@ echo "Installing ChipDip DACs..."
 
 sudo cp Adding-CHIPDIP-DAC-to-Raspberry-Pi/chipdip-i2s-master-dac.dtbo /boot/overlays/chipdip-i2s-master-dac.dtbo
 sudo chmod +x Adding-CHIPDIP-DAC-to-Raspberry-Pi/./chipdip-dac-driver_v1
-sudo sed -i '/^exit 0/i ./chipdip-dac-driver_v1 &' /etc/rc.local
+sudo sed -i '/^exit 0/i Adding-CHIPDIP-DAC-to-Raspberry-Pi/./chipdip-dac-driver_v1 &' /etc/rc.local
 sudo sed -i 's/^dtparam=audio=on/#dtparam=audio=on/g' /boot/config.txt
 sudo sed -i -e '$adtoverlay=chipdip-i2s-master-dac' /boot/config.txt
 
